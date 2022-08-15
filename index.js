4a9dbea030cf86221f262b95dad076eef89d635d
bia-cmd <111292396+bia-cmd@users.noreply.github.com>
2022-08-15 11:39:32 -0300

    projeto_criado
    Bibliotecas instaladas, nodemon configurado porta definida.

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+// const porta = process.env.PORT || 3333;
+
+const porta = 3333;
+// define a porta do servidor - ou ultiliza a oferecida pelo serviço de hospedagem
+app.listen(porta, ()=> {
+      console.log('Servidor iniciado na porta:' + porta);
+
+});
