986358c360687d81bfe324a229eafa22a3ec5376
Yan Sérgio <74221297+AnnualClamp3546@users.noreply.github.com>
2022-09-09 13:21:44 -0300

    listarUsuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -4,7 +4,10 @@ const db = require("../database/connection");
 module.exports = {
     async listarUsuarios(request, response) {
         try {
-            return response.status(200).json({confirma: 'usuarios'});
+            const sql = 'SELECT usu_id, usu_nome, cur_id, usu_rm, usu_email, usu_senha, usu_foto, usu_telefone FROM usuario;';
+            const usuarios = await db.query(sql);
+
+            return response.status(200).json({confirma: 'Sucesso', nResults: usuarios[0].length, message: usuarios[0]});
 
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
