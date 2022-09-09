1868f9cc2843aab9c23d009e89095febb46bca05
Yan Sérgio <74221297+AnnualClamp3546@users.noreply.github.com>
2022-09-09 13:44:42 -0300

    listar_livGostei

--- controllers/livGosteiController.js
+++ controllers/livGosteiController.js
@@ -4,7 +4,10 @@ const db = require("../database/connection");
 module.exports ={
     async listarLivGostei(request, response){
         try{
-            return response.status(200).json({confirma: 'livros que gostei'});
+            const sql = 'SELECT liv_id, usu_id FROM liv_gostei;';
+            const livGostei = await db.query(sql);
+
+            return response.status(200).json({confirma: 'Sucesso', nResults: livGostei[0].length, message: livGostei[0]});
 
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
