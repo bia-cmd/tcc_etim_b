d0d74a3996b7b1b20f76c83c72595f04aba0dd61
NicollyCristina <110108817+NicollyCristina@users.noreply.github.com>
2022-09-09 13:47:54 -0300

    finalizado
    livros, livAvaliados

--- controllers/livAvaliadosControler.js
+++ controllers/livAvaliadosControler.js
@@ -5,8 +5,10 @@ const db = require("../database/connection");
 module.exports = {
     async listarLivAvaliados(request, response){
         try{
-            return response.status(200).json({confirma: 'livro Avaliados'});
+           const sql= 'SELECT usu_id, liv_id, liv_nota FROM liv_avalidos;';
+           const liv_avalidos = await db.query(sql);
 
+            return response.status(200).json({confirma: 'Sucesso', nResults: liv_avalidos[0].length, message: liv_avalidos[0]});
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
                 }
--- controllers/livroController.js
+++ controllers/livroController.js
@@ -6,8 +6,10 @@ const db = require("../database/connection");
 module.exports ={
     async listarLivro(request, response){
         try{
-            return response.status(200).json({confirma: 'Livro'});
+            const sql = 'SELECT liv_id, liv_nome, edit_id, gen_id, liv_quant_paginas, liv_descricao, liv_foto, liv_moderacao FROM livro;';
+            const livro = await db.query(sql);
 
+            return response.status(200).json({confirma: 'Sucesso', nResults: livro[0].length, message: livro[0]});
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
                 }
