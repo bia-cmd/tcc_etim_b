a9f20e3c359684e7c4004b834be291fcbe173593
Yan Sérgio <74221297+AnnualClamp3546@users.noreply.github.com>
2022-09-09 13:42:59 -0300

    listar_opcLivros

--- controllers/opcLivrosController.js
+++ controllers/opcLivrosController.js
@@ -4,7 +4,10 @@ const db = require("../database/connection");
 module.exports ={
     async listarOpcLivro(request, response){
         try{
-            return response.status(200).json({confirma: 'opcLivro'});
+            const sql = 'SELECT opc_id, usu_id, liv_id, opc_lendo, opc_abandonado, opc_li, opc_quero_ler, opc_relendo FROM opcLivros;';
+            const opcLivros = await db.query(sql);
+
+            return response.status(200).json({confirma: 'Sucesso', nResults: opcLivros[0].length, message: opcLivros[0]});
 
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
