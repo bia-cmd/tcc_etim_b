ecff1a29e570b32937d5183da2ea7b4d217c31f8
NicollyCristina <110108817+NicollyCristina@users.noreply.github.com>
2022-09-21 10:59:08 -0300

    terminando
    liv_avaliados, livros

--- controllers/livroController.js
+++ controllers/livroController.js
@@ -19,13 +19,9 @@ module.exports ={
     async create(request, response){
       try {
         const { livros, nome, edit, genero, quantidade, descricao, foto, moderacao } = request.body;
-
-        const sql = 'INSERT INTO livro ( liv_nome, edit_id, gen_id, liv_quant_paginas, liv_descricao, liv_foto, liv_moderacao) VALUES ( ?, ?, ?, ?);';
-
+        const sql = 'INSERT INTO livro ( liv_nome, edit_id, gen_id, liv_quant_paginas, liv_descricao, liv_foto, liv_moderacao) VALUES ( ?, ?, ?, ?, ?, ?, ?);';
         const values = [liv_id, liv_nome, edit_id, gen_id, liv_quant_paginas, liv_descricao, liv_foto, liv_moderacao];
-
         const confirmacao = await db.query(sql, values);
-
         const liv_id = confirmacao[0].insertId;
 
         return response.status(200).json({confirma: 'Sucesso', nResults: livro[0].length, message: liv_id[0]});
